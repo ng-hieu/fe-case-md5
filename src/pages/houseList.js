e76dc865f13c9a6f3b4e8b9e487cacff74ed09cf
Your Name <you@example.com>
2023-05-30 15:25:17 +0700

    da hien thi anh

--- src/pages/houseList.js
+++ src/pages/houseList.js
@@ -9,6 +9,7 @@ export function HouseList() {
     return houseList.listHouse;
   });
   console.log("house", house);
+
   useEffect(() => {
     dispatch(getAllHouse());
   }, []);
@@ -33,40 +34,39 @@ export function HouseList() {
                       <div className="item">
                         <div className="row">
                           <div className="col-lg-4 col-sm-5">
+                            
                             <div className="image">
                               <img
-                                src="./assets/images/country-01.jpg"
+                                src={(item.image[0])?item.image[0].imageURL:''}
                                 alt=""
                               />
                             </div>
                           </div>
                           <div className="col-lg-8 col-sm-7">
                             <div className="right-content">
-                              <h4>SWITZERLAND</h4>
-                              <span>Europe</span>
+                              <h4>{item.nameHouse}</h4>
+                              <span>{item.district.name}</span>
                               <p>
-                                Woox Travel is a professional Bootstrap 5 theme
-                                HTML CSS layout for your website. You can use
-                                this layout for your commercial work.
+                                {item.description}
                               </p>
                               <ul className="info">
                                 <li>
                                   <i className="fa fa-user" /> 8.66 Mil People
                                 </li>
                                 <li>
-                                  <i className="fa fa-globe" /> 41.290 km2
+                                  <i className="fa fa-globe" /> {item.area}m2
                                 </li>
                                 <li>
                                   <i className="fa fa-home" />
-                                  {item.price}
+                                  {item.price} VND
                                 </li>
                               </ul>
-                              <div className="text-button">
+                              {/* <div className="text-button">
                                 <a href="about.html">
                                   Need Directions ?{" "}
                                   <i className="fa fa-arrow-right" />
                                 </a>
-                              </div>
+                              </div> */}
                             </div>
                           </div>
                         </div>
@@ -82,7 +82,7 @@ export function HouseList() {
                   <div className="col-lg-12">
                     <div id="map">
                       <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12469.776493332698!2d-80.14036379941481!3d25.907788681148624!2m3!1f357.26927939317244!2f20.870722720054623!3f0!3m2!1i1024!2i768!4f35!3m3!1m2!1s0x88d9add4b4ac788f%3A0xe77469d09480fcdb!2sSunny%20Isles%20Beach!5e1!3m2!1sen!2sth!4v1642869952544!5m2!1sen!2sth"
+                        src="https://maps.google.com/maps?q=HaNoi&t=&z=10&ie=UTF8&iwloc=&output=embed"
                         width="100%"
                         height="550px"
                         frameBorder={0}
