23a732685b9c572e4dd04e239fd453d5c4482906
phong0710 <phonglh071099@gmail.com>
2023-05-30 10:17:38 +0700

    truoc khi pull dev

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import { Home } from "./pages/home";
 import { Login } from "./pages/user/login";
 import { Register } from "./pages/user/register";
 import { AddHouseRenting } from "./pages/owners/addHouseRensting";
+import {useSelector} from "react-redux";
 
 function App() {
+    let user = useSelector(({user}) => {
+        return user.currentUser;
+    })
   return (
-    <>
-      <Routes>
-        <Route path={"login"} element={<Login />}></Route>
-        <Route path={"register"} element={<Register />}></Route>
-        <Route path={"home"} element={<Home />}>
-          <Route path={"create"} element={<AddHouseRenting />}></Route>
-        </Route>
-      </Routes>
-    </>
+      <>
+          <Routes>
+              <Route path={'login'} element={<Login/>}/>
+              <Route path={'register'} element={<Register/>}/>
+              {
+                  user ?
+                      <>
+                          <Route path={'home'} element={<Home/>}>
+                              <Route path={"create"} element={<AddHouseRenting />}></Route>
+
+                          </Route>
+                          <Route path={"*"} element={<Navigate to={'home'}/>}/>
+                      </>
+                      :
+                      <>
+                          <Route path={"*"} element={<Navigate to={'login'}/>}/>
+                      </>
+              }
+
+          </Routes>
+      </>
   );
 }
 
