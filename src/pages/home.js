4492eecc8f22906498ae09cd47a0fabc28fcbd70
phong0710 <phonglh071099@gmail.com>
2023-05-30 16:11:26 +0700

    phong update login payload

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,7 @@
 import { Header } from "../components/Header/header";
 import { Navbar } from "../components/Navbar/navbar";
 // import { Footer } from "../components/Footer/footer";
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 export function Home() {
   return (
     <>
