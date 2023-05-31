da33916b5018b92f1039dc4d772220fd29f30250
duylo <duyvu06497@gmail.com>
2023-05-31 18:36:42 +0700

    chwa xong

--- src/App.js
+++ src/App.js
@@ -1,39 +1,43 @@
 import React from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
-import { Home } from "./pages/home";
-import { Login } from "./pages/user/login";
-import { Register } from "./pages/user/register";
-import { AddHouseRenting } from "./pages/owners/addHouseRensting";
-import { useSelector } from "react-redux";
-import { HouseList } from "./pages/houseList";
-import { Description } from "./pages/house/description";
+import {Navigate, Route, Routes} from "react-router-dom";
+import {Home} from "./pages/home";
+import {Login} from "./pages/user/login";
+import {Register} from "./pages/user/register";
+import {AddHouseRenting} from "./pages/owners/addHouseRensting";
+import {useSelector} from "react-redux";
+import {HouseList} from "./pages/houseList";
+import {Description} from "./pages/house/description";
+import {AdapterDayjs} from "@mui/x-date-pickers-pro/AdapterDayjs";
+import {LocalizationProvider} from "@mui/x-date-pickers-pro";
 
 function App() {
-  let user = useSelector(({ user }) => {
-    return user.currenState;
-  });
-  return (
-    <>
-      <Routes>
-        <Route path={"login"} element={<Login />} />
-        <Route path={"register"} element={<Register />} />
-        {user ? (
-          <>
-            <Route path={"home"} element={<Home />}>
-              <Route path={""} element={<HouseList />}></Route>
-              <Route path={":id"} element={<Description/>}></Route>
-              <Route path={"create"} element={<AddHouseRenting />}></Route>
-            </Route>
-            <Route path={"*"} element={<Navigate to={"home"} />} />
-          </>
-        ) : (
-          <>
-            <Route path={"*"} element={<Navigate to={"login"} />} />
-          </>
-        )}
-      </Routes>
-    </>
-  );
+    let user = useSelector(({user}) => {
+        return user.currenState;
+    });
+    return (
+        <>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <Routes>
+                    <Route path={"login"} element={<Login/>}/>
+                    <Route path={"register"} element={<Register/>}/>
+                    {user ? (
+                        <>
+                            <Route path={"home"} element={<Home/>}>
+                                <Route path={""} element={<HouseList/>}></Route>
+                                <Route path={":id"} element={<Description/>}></Route>
+                                <Route path={"create"} element={<AddHouseRenting/>}></Route>
+                            </Route>
+                            <Route path={"*"} element={<Navigate to={"home"}/>}/>
+                        </>
+                    ) : (
+                        <>
+                            <Route path={"*"} element={<Navigate to={"login"}/>}/>
+                        </>
+                    )}
+                </Routes>
+            </LocalizationProvider>
+        </>
+    );
 }
 
 export default App;
