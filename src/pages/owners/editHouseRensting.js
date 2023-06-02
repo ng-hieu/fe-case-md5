2beddc25202387ab03c5ad4363ce73adbe4c6279
Your Name <you@example.com>
2023-06-02 17:28:51 +0700

    da xong edit

--- src/pages/owners/editHouseRensting.js
+++ src/pages/owners/editHouseRensting.js
@@ -1,5 +1,5 @@
 import {useEffect, useState} from "react";
-import {ref, uploadBytes, getDownloadURL} from "firebase/storage";
+import {ref, getDownloadURL, uploadBytesResumable} from "firebase/storage";
 import {storage} from "../firebase";
 import {v4} from "uuid";
 import {Field, Form, Formik} from "formik";
@@ -7,36 +7,100 @@ import {useDispatch} from "react-redux";
 import {editHouseById} from "../../service/houseService";
 import {useNavigate, useParams} from "react-router-dom";
 import customAPI from "../../service/customAPI";
-import { NavbarOfUser } from "../../components/Navbar/navbarOfUser";
+import {NavbarOfUser} from "../../components/Navbar/navbarOfUser"
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import IconButton from "@mui/material/IconButton";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import CardActions from "@mui/material/CardActions";
+import * as React from "react";
+import {Box, CircularProgress, Typography} from "@mui/material";
+import PropTypes from "prop-types";
+import CardMedia from "@mui/material/CardMedia";
 
+function CircularProgressWithLabel(props) {
+    return (
+        <Box sx={{position: "relative", display: "inline-flex"}}>
+            <CircularProgress variant="determinate" {...props} />
+            <Box
+                sx={{
+                    top: 0,
+                    left: 0,
+                    bottom: 0,
+                    right: 0,
+                    position: "absolute",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}>
+                <Typography variant="caption" component="div" color="text.secondary">
+                    {`${Math.round(props.value)}%`}
+                </Typography>
+            </Box>
+        </Box>
+    );
+}
+
+CircularProgressWithLabel.propTypes = {
+    /**
+     * The value of the progress indicator for the determinate variant.
+     * Value between 0 and 100.
+     * @default 0
+     */
+    value: PropTypes.number.isRequired,
+};
 
 export function EditHouseRenting() {
     const dispatch = useDispatch();
     const navigate = useNavigate()
     const {id} = useParams()
+    const [progress, setProgress] = useState();
     const [house, setHouse] = useState({
         nameHouse: "",
         price: "",
         area: "",
         description: "",
         wards: "",
-        district: ""
+        district: "",
+        image: []
     })
     useEffect(() => {
         customAPI.get(`house/${id}`).then((res) => {
             setHouse(res.data)
         })
     }, [])
-    const [imageUrls, setImageUrls] = useState([]);
+
     const [selectedFile, setSelectedFile] = useState(null);
 
-    const uploadFile = async (setFieldValue) => {
+    const uploadFile = async (setFieldValue, values) => {
         if (!selectedFile) return;
         const imageRef = ref(storage, `images/${selectedFile.name + v4()}`);
-        await uploadBytes(imageRef, selectedFile);
-        const url = await getDownloadURL(imageRef);
-        setFieldValue("image", [...imageUrls, url]);
-        setImageUrls((prev) => [...prev, url]);
+        const uploadTask = uploadBytesResumable(imageRef, selectedFile);
+
+        uploadTask.on(
+            "state_changed",
+            (snapshot) => {
+
+                setProgress((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+            },
+            (error) => {
+                console.log(error)
+
+            },
+            () => {
+
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    console.log("File available at", downloadURL);
+                    setFieldValue("image", [...values.image, downloadURL]);
+                    setHouse({
+                        ...house,
+                        image: [...house.image, {imageURL: downloadURL}]
+                    })
+
+
+                });
+            }
+        );
 
     };
 
@@ -70,17 +134,22 @@ export function EditHouseRenting() {
             });
     }, [districtId]);
 
+
     return (
         <>
             <NavbarOfUser></NavbarOfUser>
             <Formik
-                initialValues={house}
+                initialValues={{
+                    ...house,
+                    image: house.image.map(item => item.imageURL)
+                }}
                 enableReinitialize={true}
                 onSubmit={async (values, {setSubmitting}) => {
                     await dispatch(editHouseById(
                         {
                             id: id,
                             house: values
+
                         }
                     ))
                     navigate('/home')
@@ -111,6 +180,8 @@ export function EditHouseRenting() {
                                                                 <div className="form-addHouse">
                                                                     <div className="descr-addHouse">
                                                                         EDIT HOUSE
+
+                                                                        {console.log("values:", values)}
                                                                     </div>
                                                                     <div className="input-addHouse">
                                                                         <Field
@@ -153,11 +224,11 @@ export function EditHouseRenting() {
                                                                                onChange={(e) => {
                                                                                    handleDistrictChange(e.target.value)
                                                                                    setFieldValue("district", e.target.value)
-                                                                               }} className={'district-select'}>
-                                                                            <option value='' className="district-option">Select district</option>
+                                                                               }}>
+                                                                            <option value=''>Chọn Quận</option>
                                                                             {district.map((item) => {
                                                                                 return <option key={item.id}
-                                                                                               value={item.id} className="district-option">{item.name}
+                                                                                               value={item.id}>{item.name}
                                                                                 </option>
                                                                             })
                                                                             }
@@ -165,14 +236,11 @@ export function EditHouseRenting() {
                                                                     </div>
                                                                     <div>
                                                                         <Field component={'select'} name={'wards'}
-                                                                            // onChange={(e) => {
-                                                                            //     handleDistrictChange(e.target.value)
-                                                                            // }}
-                                                                            className={'district-select'}>
-                                                                            <option value='' className="district-option">Select wards</option>
+                                                                        >
+                                                                            <option value=''>Chọn Phường/Xã</option>
                                                                             {wards.map((item) => {
                                                                                 return <option key={item.id}
-                                                                                               value={item.id} className="district-option">{item.name}
+                                                                                               value={item.id}>{item.name}
                                                                                 </option>
                                                                             })
                                                                             }
@@ -182,8 +250,48 @@ export function EditHouseRenting() {
                                                                 </div>
                                                             </div>
                                                         </div>
-
                                                         <div className="col-lg-8">
+                                                            <div className={"imgContainer"}>
+                                                                {house.image ? house.image.map((item, index) =>
+                                                                        <Card sx={{maxWidth: 345}} key={index}>
+                                                                            <CardHeader
+                                                                                title={`Image ${index + 1}`}
+                                                                                action={
+                                                                                    <IconButton aria-label="settings"
+                                                                                                onClick={() => {
+                                                                                                    let images = values.image;
+                                                                                                    images.splice(index, 1);
+                                                                                                    setFieldValue("image", images);
+
+                                                                                                    house.image.splice(index, 1)
+                                                                                                    setHouse({
+                                                                                                        ...house
+                                                                                                    })
+                                                                                                }}
+                                                                                    >
+                                                                                        <MoreVertIcon/>
+                                                                                    </IconButton>
+                                                                                }
+                                                                            />
+                                                                            <CardMedia
+                                                                                component="img"
+                                                                                height="194"
+                                                                                sx={{
+                                                                                    aspectRatio: 1 / 1,
+                                                                                    padding: "10px",
+
+                                                                                }
+                                                                                }
+                                                                                image={item.imageURL}
+                                                                                alt=""
+                                                                            />
+                                                                            <CardActions disableSpacing>
+                                                                            </CardActions>
+                                                                        </Card>
+                                                                    )
+                                                                    : ""}
+                                                            </div>
+
                                                             <div className="addimg-container">
                                                                 {selectedFile && (
                                                                     <img
@@ -191,6 +299,11 @@ export function EditHouseRenting() {
                                                                         alt="Preview"
                                                                     />
                                                                 )}
+                                                                <div>
+                                                                    <CircularProgressWithLabel
+                                                                        value={progress ? progress : ""}
+                                                                    />
+                                                                </div>
                                                                 <label htmlFor="arquivo">Upload Image:</label>
                                                                 <input
                                                                     type="file"
@@ -200,7 +313,7 @@ export function EditHouseRenting() {
                                                                 <button
                                                                     type="button"
                                                                     onClick={() => {
-                                                                        uploadFile(setFieldValue)
+                                                                        uploadFile(setFieldValue, values)
                                                                     }}
                                                                     className="inpdddut"
                                                                 >
@@ -208,6 +321,7 @@ export function EditHouseRenting() {
                                                                 </button>
                                                             </div>
                                                         </div>
+
                                                     </div>
                                                 </div>
                                             </center>
